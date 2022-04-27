e4c36c9bbe2e518bbff3cf144679ea0a6ce50160
Jacob Adams <56134537+Jadams505@users.noreply.github.com>
2022-04-26 23:33:10 -0400

    1.4 fixes

--- Tiles/TestTile.cs
+++ Tiles/TestTile.cs
@@ -53,7 +53,7 @@ namespace FoodOverhaul.Tiles
 
 			// Style 0 is for the ExampleTree sapling, and style 1 is for ExamplePalmTree, so here we check frameX to call the correct method.
 			// Any pixels before 54 on the tilesheet are for ExampleTree while any pixels above it are for ExamplePalmTree
-			if (tile.frameX < 54)
+			if (tile.TileFrameX < 54)
 				growSucess = Terraria.WorldGen.GrowTree(i, j);
 			else
 				growSucess = Terraria.WorldGen.GrowPalmTree(i, j);
--- WorldGen/Trees/AppleTree.cs
+++ WorldGen/Trees/AppleTree.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -8,16 +9,11 @@ namespace FoodOverhaul.WorldGen.Trees
     public class AppleTree : ModTree
     {
 
-        private static Mod Mod
-        {
-            get
-            {
-                return ModLoader.GetMod("FoodOverhaul");
-            }
-        }
+        private static Mod Mod => ModLoader.GetMod("FoodOverhaul");
 
-        private static Texture2D _branches = Mod.Assets.Request<Texture2D>("Assets/WorldGen/Trees/Apple_Tree_Branches").Value;
-        private static Texture2D _tops = Mod.Assets.Request<Texture2D>("Assets/WorldGen/Trees/Apple_Tree_Tops").Value;
+        private static Asset<Texture2D> _branch = Mod.Assets.Request<Texture2D>("Assets/WorldGen/Trees/Apple_Tree_Branches", AssetRequestMode.ImmediateLoad);
+        private static Asset<Texture2D> _top = Mod.Assets.Request<Texture2D>("Assets/WorldGen/Trees/Apple_Tree_Tops", AssetRequestMode.ImmediateLoad);
+
         public override int DropWood()
         {
             return ItemID.Apple;
@@ -25,17 +21,17 @@ namespace FoodOverhaul.WorldGen.Trees
 
         public override Texture2D GetBranchTextures(int i, int j, int trunkOffset, ref int frame)
         {
-            return TextureAssets.TreeBranch[3].Value;
+            return _branch?.Value;
         }
 
         public override Texture2D GetTexture()
         {
-            return TextureAssets.Wood[0].Value;
+            return TextureAssets.Tile[TileID.Trees].Value;
         }
         
         public override Texture2D GetTopTextures(int i, int j, ref int frame, ref int frameWidth, ref int frameHeight, ref int xOffsetLeft, ref int yOffset)
         {
-            return TextureAssets.TreeTop[2].Value;
+            return _top?.Value;
         }
     }
 }
